508f1b1ad3759e84193a4ddcab731a419e5f73a0
TAYLOR SCOTT <TAYSCOTT@uat.edu>
2022-05-15 16:58:44 -0700

    Modified test for empty stack and push compete to assertIs

--- TestStackArray.py
+++ TestStackArray.py
@@ -7,9 +7,10 @@ s = ArrayStack()
 class MyTestCase(unittest.TestCase):
 
     def test_emptyStack(self):
-        self.assertIsNotNone(s)  # Test to see if stack is empty
+        self.assertIs(s.stack_empty(),True)  # Test to see if stack is empty
     def test_pushcomplete(self):
-        self.assertIsNotNone(s) #Test to see if stack has an integer from push
+        self.assertIs(s.stack_empty(),False) #Test to see if stack has an integer from push
+
 
 
 if __name__ == '__main__':
