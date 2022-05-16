f1a911c7edb2f786247d575fae0df590e2a0ae50
TAYLOR SCOTT <TAYSCOTT@uat.edu>
2022-05-15 17:10:15 -0700

    Created test for thrown exception when pop is used on empty array

--- TestStackArray.py
+++ TestStackArray.py
@@ -3,13 +3,19 @@ import unittest
 from StackArray import ArrayStack
 
 s = ArrayStack()
+def pop_error():
+    raise Exception('Array is empty')
 
 class MyTestCase(unittest.TestCase):
 
-    def test_emptyStack(self):
-        self.assertIs(s.stack_empty(),True)  # Test to see if stack is empty
-    def test_pushcomplete(self):
-        self.assertIs(s.stack_empty(),False) #Test to see if stack has an integer from push
+    def test_emptyStack(self):  # Test to see if stack is empty
+        self.assertIs(s.stack_empty(),True)
+    def test_pushcomplete(self):    # Test to see if stack has an integer from push
+        self.assertIs(s.stack_empty(),False)
+    def test_pop(self): # Test to make sure exception is thrown is for attempts to pop empty array
+        with self.assertRaises(Exception) as context:
+            pop_error()
+        self.assertTrue('Array is empty' in context.exception)
 
 
 
